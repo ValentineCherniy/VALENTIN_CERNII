c1efc3b4433375245878177580f7bd4edf9e6538
ValentineCHerniy <cherniy.valentin@gmail.com>
2024-11-05 18:40:39 +0200

    Created Package and Class Displayed string "HW!" Asked variables initiated with their value and displayed in terminal

---
+++ src/main/java/homework_nr_2/HomeWork2.java
@@ -0,0 +1,19 @@
+package homework_nr_2;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
+        boolean JavaIsAmazing = true;
+        float Pi = 3.14159F;
+
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterInAlphabet);
+        System.out.println(JavaIsAmazing);
+        System.out.println(Pi);
+
+    }
+}
