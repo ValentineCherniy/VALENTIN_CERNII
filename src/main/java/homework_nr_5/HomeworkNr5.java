8beb8f6feda0975c647a0f4ddf9a65062dd66210
ValentineCHerniy <cherniy.valentin@gmail.com>
2024-11-11 23:51:35 +0200

    create calculation on even and odd massive's numbers

--- src/main/java/homework_nr_5/HomeworkNr5.java
+++ src/main/java/homework_nr_5/HomeworkNr5.java
@@ -2,13 +2,26 @@ package homework_nr_5;
 
 public class HomeworkNr5 {
     public static void main(String[] args) {
+        int sumOfEven = 0;
+        long multOfOdd = 1;
         int[] numbers = new int[101];
         System.out.println("Array \"Numbers\":");
         for ( int i = 0; i < numbers.length; i++) {
             numbers[i] = 100 - i;
             System.out.print(numbers[i] + " ");
+            if (numbers[i] % 2 == 0){
+                sumOfEven += numbers[i];
+            } else {
+                multOfOdd *= numbers[i];
+            }
         }
         System.out.println();
+        System.out.println("===========");
+        System.out.println("Summary of even numbers in array \"Numbers\": " + sumOfEven);
+        System.out.println("Multiplication of odd numbers in array \"Numbers\": " + multOfOdd);
+        System.out.println("===========");
+
+
 
         int[] copyNumbers = new int[numbers.length];
         System.out.println("Array \"copyNumbers\":");
