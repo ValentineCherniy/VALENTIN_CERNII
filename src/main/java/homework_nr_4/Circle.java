ba972f9066e57c21907e243dde947073e65be81f
ValentineCHerniy <cherniy.valentin@gmail.com>
2024-11-11 21:39:29 +0200

    changed names of variables to small letter

--- src/main/java/homework_nr_4/Circle.java
+++ src/main/java/homework_nr_4/Circle.java
@@ -2,13 +2,13 @@ package homework_nr_4;
 
 public class Circle {
     public static void main(String[] args) {
-        float Radius = 5.5F;
-        double Area = calculateArea(Radius);
-        System.out.println("The area of a circle with radius " + Radius + " units is " + Area + " units^2");
+        float radius = 5.5F;
+        double area = calculateArea(radius);
+        System.out.println("The area of a circle with radius " + radius + " units is " + area + " units^2");
 
     }
     public static double calculateArea(float inputRadius){
-        double CalculatingAreaOfCircle = Math.PI * Math.pow(inputRadius, 2);
-        return CalculatingAreaOfCircle;
+        double calculatingAreaOfCircle = Math.PI * Math.pow(inputRadius, 2);
+        return calculatingAreaOfCircle;
     }
 }
