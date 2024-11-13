f78ef425cf93f50f50b1af228777c5d77f0698c5
ValentineCHerniy <cherniy.valentin@gmail.com>
2024-11-13 23:34:27 +0200

    add temperature converter

--- src/main/java/homework_nr_4/Main.java
+++ src/main/java/homework_nr_4/Main.java
@@ -5,8 +5,16 @@ public class Main {
         float radiusM = 5.5F;
         double area = calculateArea(radiusM);
         System.out.println("The area of a circle1 with radius " + radiusM + " units is " + area + " units^2");
+
         float radiusC = 55F;
         System.out.println("The area of a circle2 with radius " + radiusC + " units is " + Circle.CircleArea(radiusC) + " units^2");
+
+        System.out.println();
+        int c = 3;
+        System.out.println(c + "\u00B0" +"C is " + TemperatureConverter.toFahrenheit(c) + "F");
+
+        int f = 205;
+        System.out.println(f + "F is " + TemperatureConverter.toCelsius(f) + "\u00B0" +"C");
 
     }
     public static double calculateArea(float inputRadius){
