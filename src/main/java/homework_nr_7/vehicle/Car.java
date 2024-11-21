ee8ca2085aba2b3a9c2e75d7246467f3e98a0896
ValentineCHerniy <cherniy.valentin@gmail.com>
2024-11-21 17:44:43 +0200

    Add Motorcycle and Car classes as children for Vehicle class. Create objects and output their parameters

---
+++ src/main/java/homework_nr_7/vehicle/Car.java
@@ -0,0 +1,35 @@
+package homework_nr_7.vehicle;
+
+public class Car extends Vehicle{
+
+    private int numberOfDoors = 4;
+    private boolean isElectric = false;
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public boolean getIsElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(boolean electric) {
+        isElectric = electric;
+    }
+
+
+
+    public Car(String brand, String model, int year, int numberOfDoors, boolean isElectric) {
+        super(brand, model, year);
+        this.numberOfDoors = numberOfDoors;
+        this.isElectric = isElectric;
+    }
+
+
+
+
+}
---
+++ src/main/java/homework_nr_7/vehicle/VehicleMain.java
@@ -0,0 +1,13 @@
+package homework_nr_7.vehicle;
+
+public class VehicleMain {
+    public static void main(String[] args) {
+        Car c_1 = new Car("Tesla", "Model S", 2023, 4, true);
+        Motorcycle m_1 = new Motorcycle("Yamaha", "YZF-R3", 2021, 321, true);
+        Motorcycle m_2 = new Motorcycle("Honda", "VT-750AC", 2004, 749, false);
+
+        System.out.println(c_1.getBrand() + " " + c_1.getModel() + " " + c_1.getYear() + ", doors: " + c_1.getNumberOfDoors() + ", electric: " + c_1.getIsElectric());
+        System.out.println(m_1.getBrand() + " " + m_1.getModel() + " " + m_1.getYear() + ", " + m_1.getEngineCapacity() + "cc, ABS:" + m_1.isHasAbs());
+        System.out.println(m_2.getBrand() + " " + m_2.getModel() + " " + m_2.getYear() + ", " + m_2.getEngineCapacity() + "cc, ABS:" + m_2.isHasAbs());
+    }
+}
